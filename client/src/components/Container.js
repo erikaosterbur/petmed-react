bd08fc57d01967b0f23e8d9080134565a3a2b49a
Erika Osterbur <erikaosterbur@Erikas-iMac.local>
2021-12-16 15:28:26 -0600

    create pet form

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,7 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './Header';
 import Login from './pages/Login';
+import PetDashboard from "./pages/PetDashboard";
 import CreatePet from './pages/CreatePet';
+import CreateVet from './pages/CreateVet';
+import UpdatePet from './pages/UpdatePet';
+import UpdateVet from './pages/UpdateVet';
+import PetProfile from './pages/PetProfile';
+import Account from './pages/Account';
 import Footer from './Footer';
 
 export default function Container() {
@@ -13,7 +19,13 @@ export default function Container() {
         <Login />
         <Router>
           <Routes>
-            <Route exact path="/newpet" element={<CreatePet />} />
+            <Route exact path="/home" element={<PetDashboard />} />
+            <Route exact path="/new-pet" element={<CreatePet />} />
+            <Route exact path="/new-vet-visit" element={<CreateVet />} />
+            <Route exact path="/:id/edit-pet" element={<UpdatePet />} />
+            <Route exact path="/:id/edit-vet-visit" element={<UpdateVet />} />
+            <Route exact path="/:id/pet-profile" element={<PetProfile />} />
+            <Route exact path="/:id/account" element={<Account />} />
           </Routes>
         </Router>
         <Footer />
