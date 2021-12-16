38161b07af0bacc8d8ed4c637b18d176a23af589
Erika Osterbur <erikaosterbur@Erikas-iMac.local>
2021-12-16 16:50:59 -0600

    footer fix

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer({ currentPage, handlePageChange }) {
     return (
         <footer>
-            <p className="d-flex justify-content-center" style={{color: "white"}}>©PetMed 2021</p>
+            <p className="d-flex justify-content-center m-2" style={{color: "white"}}>©PetMed 2021</p>
         </footer>
 
     )
