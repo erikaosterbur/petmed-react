f9591d15906c15737b6fb79e1ceffbed0a364540
Erika Osterbur <erikaosterbur@Erikas-iMac.local>
2021-12-16 19:49:51 -0600

    pet profile page started

--- client/src/components/pages/PetProfile.js
+++ client/src/components/pages/PetProfile.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Ginny from '../../images/ginny.jpg';
+import { Card } from 'react-bootstrap';
+
 
 function PetProfile() {
     return (
         <>
-        
+            {/* TO DO: Change to reflect pet id in params and get info from database */}
+            <h1 className="d-flex justify-content-center p-4">Ginny</h1>
+            <div className="row">
+                <img src={Ginny} style={{width: "30%", border: "1rem solid white"}} className="m-5" />
+                <Card className="col-6">
+                    <Card.Body>
+                        Type: Dog <br></br>
+                        Date of Birth: August 21st, 2016 <br></br>
+                        Breed: Shepherd Mix
+                    </Card.Body>
+                </Card>
+            </div>
         </>
     )
 }
